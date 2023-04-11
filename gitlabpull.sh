c776a3b29636fab203aaa7a303b37414b9f8c4a9
Seno Anggoro <seno@qtasnim.com>
2023-04-11 08:16:38 +0700

    Update gitlabpull.sh

--- gitlabpull.sh
+++ gitlabpull.sh
@@ -6,6 +6,6 @@ if [ "update" = $CI_JOB_STAGE ]; then
     #fase update server qc
     ssh -o StrictHostKeyChecking=no "${SSH_USER_STAGING}@${SSH_STAGING}" bash <<EOF
     cd /data/users/qc/sources/pwm-auth/ci
-    //git pull
+    git pull
 EOF
 fi
