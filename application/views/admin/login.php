45b6d0b48e68313349bbd61fec942248b9a38f4d
MicroThunder <nurdin.reverse73@gmail.com>
2023-04-18 10:25:10 +0700

    penyesuaian login sippmu

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 11]>
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 11]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
-	<!-- Meta -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>SIMKATMUH - Aplikasi Pembukuan Akuntansi Keuangan Digital </title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title><?= env('APP_NAME') ?> </title>
     <meta name="description" content="Pencatatan dan laporan keuangan usaha dengan aplikasi keuangan SIMKATMUH" />
-	
+
     <link href="<?php echo base_url(); ?>assets/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
 
 </head>
+
 <body>
     <div class="auth-wrapper align-items-stretch aut-bg-img">
         <div class="flex-grow-1">
             <div class="h-20 d-md-flex align-items-center auth-side-img">
                 <div class="col-sm-3 auth-content w-auto">
-                    <img src="<?php echo base_url(); ?>assets/images/muhammadiyah-logo.jpg" alt="" class="img-fluid logo-muh">
+                    <img src="<?php echo base_url(); ?>assets/images/muhammadiyah-logo.jpg" alt=""
+                        class="img-fluid logo-muh">
                 </div>
             </div>
             <div class="auth-side-form">
                 <div class=" auth-content">
-                    <div class="xcenter"><img src="<?php echo base_url(); ?>assets/images/logo2.png" alt="" class="img-fluid mb-4" style="height:100px"></div>
-                    <h4 class="mb-2 f-w-400">SIMKATMUH SEKOLAH</h4>
+                    <div class="xcenter"><img src="<?php echo base_url(); ?>assets/images/logo2.png" alt=""
+                            class="img-fluid mb-4" style="height:100px"></div>
+                    <h4 class="mb-2 f-w-400"><?= env('APP_NAME') ?></h4>
                     <h4 class="mb-4 f-w-400">PDM KABUPATEN CIREBON</h4>
                     <?php
                     if (isset($error_message)) {
@@ -43,7 +47,7 @@
                     ?>
                     <form action="<?php echo site_url('auth2/login') ?>" method="post">
                         <?php echo $this->customlib->getCSRF(); ?>
-                        
+
                         <div class="form-group mb-3">
                             <label class="floating-label" for="username">Username</label>
                             <input type="text" class="form-control" name="username" placeholder="">
@@ -61,11 +65,12 @@
             </div>
         </div>
     </div>
-    
+
     <script src="<?php echo base_url(); ?>assets/js/vendor-all.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/plugins/bootstrap.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/ripple.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/pcoded.min.js"></script>
-    
+
 </body>
+
 </html>
